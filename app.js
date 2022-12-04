4897d4055a164c65033c3a320b531f240a977277
Leon Acero <acero@hotmail.com>
2022-12-04 14:36:34 -0600

    prueba para corregir deploy en render.com #2

--- app.js
+++ app.js
@@ -1330,9 +1330,9 @@ console.log("dirname", __dirname);
 
 // Y aqui con el * le digo que cuando le lleue cualquier request lo va a redireccionar a este path
 
-// app.get('*', (req, res) => {
-  // res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 ///////////////////////////////////////////////////////////////////
 
