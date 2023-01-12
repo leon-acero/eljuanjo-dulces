c6501669996077faae120dcd3c2bf84cea060060
Leon Acero <acero@hotmail.com>
2023-01-11 22:49:38 -0600

    404 Error

--- app.js
+++ app.js
@@ -1391,19 +1391,19 @@ app.all('*', (req, res, next) => {
   ///////////////////////////////////////////////////////////////////
   // Lecture-115 Better Errors and Refactoring
   ///////////////////////////////////////////////////////////////////
-	next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+	// next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 
-  // res.status(404)
+  res.status(404)
 
-  // if (req.accepts('html')) {
-  //     res.sendFile(path.join(__dirname, 'views', '404.html'))
-  // } 
-  // else if (req.accepts('json')) {
-  //     res.json({ message: '404 Not Found' })
-  // } 
-  // else {
-  //     res.type('txt').send('404 Not Found')
-  // }
+  if (req.accepts('html')) {
+      res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } 
+  else if (req.accepts('json')) {
+      res.json({ message: '404 Not Found' })
+  } 
+  else {
+      res.type('txt').send('404 Not Found')
+  }
 });
 
 
